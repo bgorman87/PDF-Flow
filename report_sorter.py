8d9a9a35829dc0a9e0ce0b298202207e012e3ab4
bgorm <bgorman@live.com>
2022-10-26 20:41:39 -0300

    database test and proper for formatting spaces

--- report_sorter.py
+++ report_sorter.py
@@ -31,7 +31,7 @@ class MainWindow(QtWidgets.QMainWindow):
         super(MainWindow, self).__init__(*args, **kwargs)
         object.__init__(self)
         self.analyzeWorker = None
-        self.fileNames = None 
+        self.fileNames = None
         self.analyzed = False
         self.progress = 0
         self.centralwidget = QtWidgets.QWidget()
@@ -64,7 +64,8 @@ class MainWindow(QtWidgets.QMainWindow):
         self.project_numbers_short = []
 
         self.threadpool = QtCore.QThreadPool()
-        print("Multithreading with maximum %d threads" % self.threadpool.maxThreadCount())
+        print("Multithreading with maximum %d threads" %
+              self.threadpool.maxThreadCount())
 
     # def setup_ui(self, main_window):
         self.setObjectName("MainWindow")
@@ -77,7 +78,8 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.tabWidget = QtWidgets.QTabWidget(self.centralwidget)
         self.tabWidget.setObjectName("tabWidget")
-        self.tabWidget.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        self.tabWidget.setSizePolicy(
+            QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
         self.layoutGrid.addWidget(self.tabWidget, 0, 0, 1, 1)
 
         self.label = QtWidgets.QLabel(self.centralwidget)
@@ -151,7 +153,8 @@ class MainWindow(QtWidgets.QMainWindow):
         self.graphicsView.setGeometry(QtCore.QRect(10, 160, 320, 400))
         self.graphicsView.setObjectName("graphicsView")
         self.gridLayout_2.addWidget(self.graphicsView, 8, 0, 20, 5)
-        self.graphicsView.setViewportUpdateMode(QtWidgets.QGraphicsView.FullViewportUpdate)
+        self.graphicsView.setViewportUpdateMode(
+            QtWidgets.QGraphicsView.FullViewportUpdate)
 
         self.tabWidget.addTab(self.tab_2, "")
         self.tabWidget.raise_()
@@ -178,10 +181,12 @@ class MainWindow(QtWidgets.QMainWindow):
     def translate_ui(self):
         _translate = QtCore.QCoreApplication.translate
         self.setWindowTitle(_translate("MainWindow", "Englobe Sorter"))
-        self.label.setText(_translate("MainWindow", "Created By Brandon Gorman"))
+        self.label.setText(_translate(
+            "MainWindow", "Created By Brandon Gorman"))
         self.SelectFiles.setText(_translate("MainWindow", "Select Files"))
         self.SelectFiles.clicked.connect(self.select_files_handler)
-        self.fileRenameButton.setWhatsThis(_translate("MainWindow", "Rename the currently selected file"))
+        self.fileRenameButton.setWhatsThis(_translate(
+            "MainWindow", "Rename the currently selected file"))
         self.fileRenameButton.setText(_translate("MainWindow", "Rename"))
         self.fileRenameButton.clicked.connect(self.file_rename_button_handler)
         self.analyzeButton.setText(_translate("MainWindow", "Analyze"))
@@ -189,16 +194,20 @@ class MainWindow(QtWidgets.QMainWindow):
         self.emailButton.setText(_translate("MainWindow", "E-Mail"))
         self.debugBox.setText(_translate("MainWindow", "Debug"))
         self.emailButton.clicked.connect(self.email_button_handler)
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("MainWindow", "Input"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(
+            self.tab), _translate("MainWindow", "Input"))
         self.label_3.setText(_translate("MainWindow", "File Output Viewer:"))
         self.label_4.setText(_translate("MainWindow", "Combined Files:"))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), _translate("MainWindow", "Output"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(
+            self.tab_2), _translate("MainWindow", "Output"))
         self.listWidget.itemClicked.connect(self.list_widget_handler)
         self.listWidget.itemDoubleClicked.connect(self.rename_file_handler)
 
     def email_button_handler(self):
-        signature_path = os.path.abspath(os.path.join(home_dir + r"\\Signature\\concrete.htm"))
-        signature_path_28 = os.path.abspath(os.path.join(home_dir + r"\\Signature\\concrete28.htm"))
+        signature_path = os.path.abspath(os.path.join(
+            home_dir + r"\\Signature\\concrete.htm"))
+        signature_path_28 = os.path.abspath(os.path.join(
+            home_dir + r"\\Signature\\concrete28.htm"))
         if not self.analyzed and self.fileNames:
             if os.path.isfile(signature_path):
                 with open(signature_path, "r") as file:
@@ -232,7 +241,8 @@ class MainWindow(QtWidgets.QMainWindow):
                             dexter_number = re.search(r"Dexter_([\d-]*)[_\dA-z]", email_recipient_subject,
                                                       re.I).groups()
                             dexter_number = dexter_number[-1]
-                        email_recipient_subject = email_recipient_subject.replace("%%", dexter_number)
+                        email_recipient_subject = email_recipient_subject.replace(
+                            "%%", dexter_number)
                     try:
                         outlook = win32.Dispatch('outlook.application')
                         mail = outlook.CreateItem(0)
@@ -264,11 +274,13 @@ class MainWindow(QtWidgets.QMainWindow):
             all_list_titles = []
             all_list_data = []
             for i in range(self.listWidget.count()):
-                all_list_data.append(self.listWidget.item(i).data(QtCore.Qt.UserRole).split("%%")[0])
+                all_list_data.append(self.listWidget.item(
+                    i).data(QtCore.Qt.UserRole).split("%%")[0])
                 all_list_titles.append(self.listWidget.item(i).text())
             for i, project_number in enumerate(self.project_numbers):
                 if self.project_numbers_short[i][0] == "P":
-                    self.project_numbers_short[i] = self.project_numbers_short[i].replace("P-", "P-00")
+                    self.project_numbers_short[i] = self.project_numbers_short[i].replace(
+                        "P-", "P-00")
                 project_number, project_number_short, \
                     recipients, recipients_cc, subject = project_info(project_number, self.project_numbers_short[i],
                                                                       all_list_data[i], None, self.analyzed)
@@ -276,7 +288,8 @@ class MainWindow(QtWidgets.QMainWindow):
                 if "Dexter" in subject:
                     dexter_number = "NA"
                     if re.search(r"Dexter_([\d-]*)[_\dA-z]", all_list_titles[i], re.I) is not None:
-                        dexter_number = re.search(r"Dexter_([\d-]*)[_\dA-z]", all_list_titles[i], re.I).groups()
+                        dexter_number = re.search(
+                            r"Dexter_([\d-]*)[_\dA-z]", all_list_titles[i], re.I).groups()
                         dexter_number = dexter_number[-1]
                     subject = subject.replace("%%", dexter_number)
                 try:
@@ -309,21 +322,25 @@ class MainWindow(QtWidgets.QMainWindow):
         self.open_file_dialog()
 
     def open_file_dialog(self):
-        self.dialog = QtWidgets.QFileDialog(directory=str(os.path.abspath(os.path.join(os.getcwd(), r"..\.."))))
+        self.dialog = QtWidgets.QFileDialog(directory=str(
+            os.path.abspath(os.path.join(os.getcwd(), r"..\.."))))
         self.fileNames, filters = QtWidgets.QFileDialog.getOpenFileNames()
 
         tuple(self.fileNames)
         if len(self.fileNames) == 1:
-            file_names_string = "(" + str(len(self.fileNames)) + ")" + " file has been selected: \n"
+            file_names_string = "(" + str(len(self.fileNames)) + \
+                ")" + " file has been selected: \n"
         else:
-            file_names_string = "(" + str(len(self.fileNames)) + ")" + " files have been selected: \n"
+            file_names_string = "(" + str(len(self.fileNames)) + \
+                ")" + " files have been selected: \n"
         for item in self.fileNames:
             file_names_string = file_names_string + item + "\n"
         self.outputBox.appendPlainText(file_names_string)
 
     def rename_file_handler(self):
         if self.listWidget.isPersistentEditorOpen(self.listWidget.currentItem()):
-            self.listWidget.closePersistentEditor(self.listWidget.currentItem())
+            self.listWidget.closePersistentEditor(
+                self.listWidget.currentItem())
             self.listWidget.editItem(self.listWidget.currentItem())
         else:
             self.listWidget.editItem(self.listWidget.currentItem())
@@ -331,7 +348,8 @@ class MainWindow(QtWidgets.QMainWindow):
     def file_rename_button_handler(self):
         file_path = self.listWidget.currentItem().data(QtCore.Qt.UserRole).split("%%")
         file_path_transit_src = file_path[0]
-        file_path_project_src = file_path[1]  # Project path may be changed if project number updated so declare up here
+        # Project path may be changed if project number updated so declare up here
+        file_path_project_src = file_path[1]
 
         # See if project number is the edited string. If it is and description is == "SomeProjectDescription"
         # Then the project was previously not detected properly so assume the project edit is correct and find
@@ -348,12 +366,14 @@ class MainWindow(QtWidgets.QMainWindow):
         project_details_changed = False
         new_title = self.fileRename.text()
         if re.search(r"-([\dPBpb\.-]+)_", old_title, re.I) is not None:
-            old_project_number = re.search(r"-([\dPBpb\.-]+)_", old_title, re.I).groups()
+            old_project_number = re.search(
+                r"-([\dPBpb\.-]+)_", old_title, re.I).groups()
             old_project_number = old_project_number[-1]
         elif re.search(r"-(NA)_", old_title, re.I) is not None:
             old_project_number = "NA"
         if re.search(r"-([\dPBpb\.-]+)_", new_title, re.I) is not None:
-            new_project_number = re.search(r"-([\dPBpb\.-]+)_", new_title, re.I).groups()
+            new_project_number = re.search(
+                r"-([\dPBpb\.-]+)_", new_title, re.I).groups()
             new_project_number = new_project_number[-1]
         if old_project_number != new_project_number:
             project_number, project_number_short, \
@@ -365,18 +385,25 @@ class MainWindow(QtWidgets.QMainWindow):
             old_package = old_package[-1]
 
         if project_details_changed:
-            updated_file_details = old_title.replace("SomeProjectDescription", description)
-            updated_package = detect_package_number(file_path_project_src, debug)[0]
-            updated_file_details = updated_file_details.replace(old_package, updated_package)
-            updated_file_details = updated_file_details.replace(old_project_number, project_number_short)
+            updated_file_details = old_title.replace(
+                "SomeProjectDescription", description)
+            updated_package = detect_package_number(
+                file_path_project_src, debug)[0]
+            updated_file_details = updated_file_details.replace(
+                old_package, updated_package)
+            updated_file_details = updated_file_details.replace(
+                old_project_number, project_number_short)
             rename_transit_len = 260 - len(
                 str(file_path_transit_src.replace(file_path_transit_src.split("\\").pop(), "")))
             rename_project_length = 260 - len(str(file_path_project_src))
             if len(updated_file_details) > rename_transit_len or len(updated_file_details) > rename_project_length:
-                updated_file_details = updated_file_details.replace("Concrete", "Conc")
+                updated_file_details = updated_file_details.replace(
+                    "Concrete", "Conc")
             if len(updated_file_details) > rename_transit_len or len(updated_file_details) > rename_project_length:
-                updated_file_details = updated_file_details.replace("-2022", "")
-                updated_file_details = updated_file_details.replace("-2021", "")
+                updated_file_details = updated_file_details.replace(
+                    "-2022", "")
+                updated_file_details = updated_file_details.replace(
+                    "-2021", "")
             if len(updated_file_details) > rename_transit_len or len(updated_file_details) > rename_project_length:
                 if rename_project_length > rename_transit_len:
                     cut = rename_project_length + 4
@@ -385,9 +412,11 @@ class MainWindow(QtWidgets.QMainWindow):
                 updated_file_details = updated_file_details.replace(".pdf", "")
                 updated_file_details = updated_file_details[:-cut] + "LONG.pdf"
             rename_path_transit = os.path.abspath(os.path.join(
-                file_path_transit_src.replace(file_path_transit_src.split("\\").pop(), ""),
+                file_path_transit_src.replace(
+                    file_path_transit_src.split("\\").pop(), ""),
                 updated_file_details + ".pdf"))
-            rename_path_project = os.path.abspath(os.path.join(file_path_project_src, updated_file_details + ".pdf"))
+            rename_path_project = os.path.abspath(os.path.join(
+                file_path_project_src, updated_file_details + ".pdf"))
             os.rename(file_path_transit_src, rename_path_transit)
             if not os.path.isfile(file_path_project_src):
                 file_path_project_src = rename_path_project
@@ -400,25 +429,33 @@ class MainWindow(QtWidgets.QMainWindow):
             data = rename_path_transit + "%%" + rename_path_project
             self.listWidget.currentItem().setData(QtCore.Qt.UserRole, data)
             self.fileRename.setText(updated_file_details)
-            self.project_numbers_short[self.listWidget.currentRow()] = project_number_short
+            self.project_numbers_short[self.listWidget.currentRow(
+            )] = project_number_short
             self.project_numbers[self.listWidget.currentRow()] = project_number
         else:
             # 254 to accommodate the .pdf
-            rename_transit_len = 254 - len(file_path_transit_src.replace(file_path_transit_src.split("\\").pop(), ""))
-            rename_project_len = 254 - len(file_path_project_src.replace(file_path_project_src.split("\\").pop(), ""))
+            rename_transit_len = 254 - \
+                len(file_path_transit_src.replace(
+                    file_path_transit_src.split("\\").pop(), ""))
+            rename_project_len = 254 - \
+                len(file_path_project_src.replace(
+                    file_path_project_src.split("\\").pop(), ""))
             if len(self.fileRename.text()) > rename_transit_len or len(self.fileRename.text()) > rename_project_len:
                 print("Filename too long")
                 if rename_transit_len > rename_project_len:
                     msg_string = f"Filename too long. Reduce by {len(self.fileRename.text()) - rename_transit_len}"
                 else:
                     msg_string = f"Filename too long. Reduce by {len(self.fileRename.text()) - rename_project_len}"
-                ctypes.windll.user32.MessageBoxW(0, msg_string, "Filename Too Long", 1)
+                ctypes.windll.user32.MessageBoxW(
+                    0, msg_string, "Filename Too Long", 1)
             else:
                 rename_path_transit = os.path.abspath(os.path.join(
-                    file_path_transit_src.replace(file_path_transit_src.split("\\").pop(), ""),
+                    file_path_transit_src.replace(
+                        file_path_transit_src.split("\\").pop(), ""),
                     str(self.fileRename.text()) + ".pdf"))
                 rename_path_project = os.path.abspath(os.path.join(
-                    file_path_project_src.replace(file_path_project_src.split("\\").pop(), ""),
+                    file_path_project_src.replace(
+                        file_path_project_src.split("\\").pop(), ""),
                     str(self.fileRename.text()) + ".pdf"))
                 try:
                     os.rename(file_path_transit_src, rename_path_transit)
@@ -431,7 +468,8 @@ class MainWindow(QtWidgets.QMainWindow):
                         pass
                 self.listWidget.currentItem().setText(self.fileRename.text())
                 if debug:
-                    print('Renamed File Path: \n{0}\n{1}'.format(file_path_transit_src, file_path_project_src))
+                    print('Renamed File Path: \n{0}\n{1}'.format(
+                        file_path_transit_src, file_path_project_src))
                 data = rename_path_transit + "%%" + rename_path_project
                 self.listWidget.currentItem().setData(QtCore.Qt.UserRole, data)
 
@@ -463,18 +501,21 @@ class MainWindow(QtWidgets.QMainWindow):
             self.data_processing()
             self.analyzed = True
         else:
-            self.outputBox.appendPlainText("Please select at least 1 file to analyze...\n")
+            self.outputBox.appendPlainText(
+                "Please select at least 1 file to analyze...\n")
         self.analyzeButton.setEnabled(True)
 
     def analyze_queue_button_handler(self):
         self.outputBox.appendPlainText("Analyzing Queue Folder...\n")
 
     def list_widget_handler(self):
-        file_path = str(self.listWidget.currentItem().data(QtCore.Qt.UserRole)).split("%%")
+        file_path = str(self.listWidget.currentItem().data(
+            QtCore.Qt.UserRole)).split("%%")
         file_path_transit_src = file_path[0]
         image_jpeg = []
         try:
-            image_jpeg = convert_from_path(file_path_transit_src, fmt="jpeg", poppler_path=poppler_path)
+            image_jpeg = convert_from_path(
+                file_path_transit_src, fmt="jpeg", poppler_path=poppler_path)
         except Exception as e:
             print(e)
         if image_jpeg:
@@ -492,7 +533,8 @@ class MainWindow(QtWidgets.QMainWindow):
             scene.addItem(item)
             self.graphicsView.setScene(scene)
             os.remove(name_jpeg)
-            set_text = file_path_transit_src.split("\\").pop().replace(".pdf", "")
+            set_text = file_path_transit_src.split(
+                "\\").pop().replace(".pdf", "")
             self.fileRename.setText(set_text)
         else:
             print("image_jpeg list is empty")
@@ -518,9 +560,12 @@ class MainWindow(QtWidgets.QMainWindow):
 
         # Import images from file path "f" using pdf to image to open
         for f in self.fileNames:
-            self.analyzeWorker = WorkerAnalyzeThread(fileName=f, debug=debug, analyzed=self.analyzed)
-            self.analyzeWorker.analyze_progress.connect(self.evt_analyze_progress)
-            self.analyzeWorker.analyze_complete.connect(self.evt_analyze_complete)
+            self.analyzeWorker = WorkerAnalyzeThread(
+                fileName=f, debug=debug, analyzed=self.analyzed)
+            self.analyzeWorker.analyze_progress.connect(
+                self.evt_analyze_progress)
+            self.analyzeWorker.analyze_complete.connect(
+                self.evt_analyze_complete)
             self.analyzeWorker.start()
 
 
