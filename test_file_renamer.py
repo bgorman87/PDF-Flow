b0cb177fbbaf2b86961a04162e13099b5e1b046a
Brandon Gorman <bgorman@live.com>
2023-10-21 11:54:20 -0300

    ignores folders

--- test_file_renamer.py
+++ test_file_renamer.py
@@ -5,6 +5,7 @@ test_files_folder = os.path.join(cwd, "test_files")
 
 for file_name in os.listdir(test_files_folder):
     if "pdf" not in file_name[-4:].lower():
+        continue
         os.remove(os.path.join(test_files_folder , file_name))
     else:
         while True:
