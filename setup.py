64ea9ceacfa844d0c5c0c33d4060ec5a8140bd11
Brandon <bgorman@live.com>
2024-01-08 15:26:24 -0400

    installer fixes

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from cx_Freeze import Executable, setup
 build_exe_options = {
     "bin_excludes": ["libqpdf.so", "libqpdf.dylib"],
     "excludes": [],
-    "include_files": [("style/styles.qss", "style/styles.qss"), ("assets/", "assets/"), ("tesseract/", "tesseract/"), ("poppler/", "poppler/")],
+    "include_files": [("style/styles.qss", "style/styles.qss"), ("assets/", "assets/"), ("Tesseract/", "Tesseract/"), ("poppler/", "poppler/")],
     "zip_include_packages": ["PySide6", "shiboken6", "pysocks"],
 }
 
@@ -17,7 +17,7 @@ executables = [Executable("main_view.py", base="Win32GUI", icon="assets/icons/ic
 # Setup configuration
 setup(
     name="PDF Flow",
-    version="0.1",
+    version="0.1.0",
     description="PDF Processing Tool",
     options={"build_exe": build_exe_options},
     executables=executables,
