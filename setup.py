8ea58a09eca8fba91e48ea978ab58f154b80de22
Brandon <bgorman@live.com>
2024-01-09 10:44:58 -0400

    cx freeze setup

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 """
 A simple setup script to create an executable using PySide6.
 """
+# python setup.py bdist_msi    # to build a Windows installer
+# Ensure pdf2image.py has Popen creation flags set to: creationflags=0x08000000
+
 from cx_Freeze import Executable, setup
 
 # Additional options
